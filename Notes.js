a6daf4a76736b5e237064b300d112549d6b34234
Yongwoo Lee <yw6636@gmail.com>
2023-07-06 15:34:45 -0400

    wip

---
+++ Notes.js
@@ -0,0 +1,21 @@
+// Designing the data table 
+// 1. Recipes: recipe_id (primary key), recipe_name 
+// 2. Ingredients: ingredient_id (primary key), ingredient_name, ingredient_unit, 
+// 3. Steps: step_id (primary key), step_text, step_order, recipe_id (foreign key)  
+// 4. Step_Ingredients (Intermediary Table): SI_id (primary key), step_id (foreign key), ingredient_id (foreign key),
+//    Quantity 
+// Foreign key can't be made for ingredients since each ingredient is NOT limited to one specific
+// step or recipe
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
